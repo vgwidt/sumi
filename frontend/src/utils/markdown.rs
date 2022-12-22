4422aae4c981d9f2f6db2a6cfc041c82c1bee5ce
vgwidt <vgwidt@live.com>
2022-12-22 16:08:39 +0900

    Sanitize markdown_to_html with Ammonia

--- frontend/src/utils/markdown.rs
+++ frontend/src/utils/markdown.rs
@@ -1,4 +1,5 @@
 use pulldown_cmark::Parser;
+use ammonia::clean;
 use web_sys::Node;
 use yew::{virtual_dom::VNode, Html};
 
@@ -7,13 +8,16 @@ pub fn markdown_to_html(body: &str) -> Html {
     let mut html_text = String::new();
     pulldown_cmark::html::push_html(&mut html_text, parser);
 
+    //Sanitize
+    let html_text = clean(&*html_text);
+
     let div = web_sys::window()
         .unwrap()
         .document()
         .unwrap()
         .create_element("div")
         .unwrap();
-    div.set_inner_html(html_text.as_str());
+    div.set_inner_html(&html_text);
     let node = Node::from(div);
     VNode::VRef(node)
 }
