de2ed48a4e3fed2ac9722f980570c54951ee3e55
vgwidt <vgwidt@live.com>
2023-01-19 16:27:38 +0900

    Accommodations for chrono-tz filter-by-regex Don't try to use Tz::UTC if no preferred timezone found Enable feature on crate

--- frontend/src/utils/timezone.rs
+++ frontend/src/utils/timezone.rs
@@ -1,15 +1,15 @@
 use chrono::{NaiveDateTime, TimeZone};
+use chrono_tz::Tz;
 
 pub fn display_time(time: &NaiveDateTime, timezone: &Option<String>, format: &str) -> String {
-    let tz = match timezone {
-        Some(user_tz) => user_tz.parse().unwrap(),
-        None => chrono_tz::Tz::UTC,
-    };
+    if let Some(user_tz) = timezone {
+        let tz: Tz = user_tz.parse().unwrap();
+        let tz_aware = tz.from_utc_datetime(time);
 
-    log::info!("Timezone: {}", tz.to_string());
-
-    let tz_aware = tz.from_utc_datetime(time);
-
-    tz_aware.format(format).to_string()
+        tz_aware.format(format).to_string()
+    }
+    else {
+        time.format(format).to_string()
+    }
     
 }
