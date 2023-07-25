3fa82443ed51d45aa566f9c933c9903269f480a6
vance <vgwidt@live.com>
2023-07-25 09:54:07 +0900

    Convert frontend to binary (#47)

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM rust:latest as base
-RUN cargo install --version 0.16.0 trunk
+RUN cargo install trunk
 RUN rustup target add wasm32-unknown-unknown
 
 FROM base as cacher
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -12,8 +12,7 @@ use wasm_bindgen::prelude::*;
 
 use app::App;
 
-#[wasm_bindgen(start)]
-pub fn run_app() -> Result<(), JsValue> {
+pub fn main() -> Result<(), JsValue> {
     wasm_logger::init(wasm_logger::Config::default());
     yew::Renderer::<App>::new().render();
     Ok(())
