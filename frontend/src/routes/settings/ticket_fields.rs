7ff3739accc7526d2ccbe21ceb7f7775c6126518
vance <vgwidt@live.com>
2023-07-28 11:55:59 +0900

    Adjust columns

--- frontend/src/routes/settings/ticket_fields.rs
+++ frontend/src/routes/settings/ticket_fields.rs
@@ -18,6 +18,7 @@ pub fn ticket_fields() -> Html {
 
     let onclick_add_field = {
         //will call call a new ticket field component with new flag
+        
     };
 
 
@@ -66,6 +67,7 @@ pub struct NewFieldProps {
 
 #[function_component(NewTicketField)]
 pub fn new_ticket_field(props: &NewFieldProps) -> Html {
+    let language = use_language_context();
     let submitted = use_state(|| false);
     let error = use_state(|| String::new());
     let update_info = use_state(|| props.field.clone());
@@ -84,8 +86,8 @@ pub fn new_ticket_field(props: &NewFieldProps) -> Html {
                 field_name: update_info.field_name.clone(),
                 field_type: update_info.field_type.clone(),
                 field_size: update_info.field_size.clone(),
-                is_select: update_info.is_select.clone(),
-                select_values: update_info.select_values.clone(),            
+                select_values: update_info.select_values.clone(),   
+                order_index: update_info.order_index.clone(),         
             };
             wasm_bindgen_futures::spawn_local(async move {
                 submitted.set(true);
@@ -109,15 +111,56 @@ pub fn new_ticket_field(props: &NewFieldProps) -> Html {
         })
     };
 
-    // let oninput_label = {
-    //     let update_info = update_info.clone();
-    //     Callback::from(move |e: InputEvent| {
-    //         let input: HtmlInputElement = e.target_unchecked_into();
-    //         let mut info = (*update_info).clone();
-    //         info.label = input.value();
-    //         update_info.set(info);
-    //     })
-    // };
+    let oninput_name = {
+        let update_info = update_info.clone();
+        Callback::from(move |e: InputEvent| {
+            let input: HtmlInputElement = e.target_unchecked_into();
+            let mut info = (*update_info).clone();
+            info.field_name = input.value();
+            update_info.set(info);
+        })
+    };
+
+    let oninput_type = {
+        let update_info = update_info.clone();
+        Callback::from(move |e: InputEvent| {
+            let input: HtmlInputElement = e.target_unchecked_into();
+            let mut info = (*update_info).clone();
+            info.field_type = input.value();
+            update_info.set(info);
+        })
+    };
+
+    let oninput_size = {
+        let update_info = update_info.clone();
+        Callback::from(move |e: InputEvent| {
+            let input: HtmlInputElement = e.target_unchecked_into();
+            let mut info = (*update_info).clone();
+            info.field_size = input.value().parse::<i32>().unwrap();
+            update_info.set(info);
+        })
+    };
+
+    let oninput_select_values = {
+        let update_info = update_info.clone();
+        Callback::from(move |e: InputEvent| {
+            let input: HtmlInputElement = e.target_unchecked_into();
+            let mut info = (*update_info).clone();
+            //split by enter
+            info.select_values = Some(input.value().split("\n").map(|s| Some(s.to_string())).collect());
+            update_info.set(info);
+        })
+    };
+
+    let oninput_order_index = {
+        let update_info = update_info.clone();
+        Callback::from(move |e: InputEvent| {
+            let input: HtmlInputElement = e.target_unchecked_into();
+            let mut info = (*update_info).clone();
+            info.order_index = input.value().parse::<i32>().unwrap();
+            update_info.set(info);
+        })
+    };
 
     // let oninput_check = {
     //     let update_info = update_info.clone();
@@ -130,10 +173,34 @@ pub fn new_ticket_field(props: &NewFieldProps) -> Html {
     // };
     
     html! {
-        <div class="task">
+        <div class="add-ticket-field">
             <form onsubmit={onclick_save}>
-                // <input type="checkbox" checked={update_info.is_enabled.clone()} oninput={oninput_check} />
-                // <input type="text" value={update_info.label.clone()} oninput={oninput_label}  placeholder="New field" />
+                //Ask for field type (select box), field name, field size, select values (if applicable), order index
+                <label>
+                    {language.get("Field Type")}
+                    <select oninput={oninput_type}>
+                        <option value="text">{language.get("Text")}</option>
+                        <option value="number">{language.get("Number")}</option>
+                        <option value="date">{language.get("Date")}</option>
+                        <option value="select">{language.get("Select")}</option>
+                    </select>
+                </label>
+                <label>
+                    {language.get("Field Name")}
+                    <input type="text" name="field_name" required=true oninput={oninput_name} />
+                </label>
+                <label>
+                    {language.get("Field Size")}
+                    <input type="number" name="field_size" required=true oninput={oninput_size} />
+                </label>
+                <label>
+                    {language.get("Select Values")}
+                    <input type="textbox" name="select_values" oninput={oninput_select_values} />
+                </label>
+                <label>
+                    {language.get("Order Index")}
+                    <input type="number" name="order_index" required=true oninput={oninput_order_index} />
+                </label>
                 <button class="page-btn" type="submit">{"✔"}</button>
             </form>
         </div>
