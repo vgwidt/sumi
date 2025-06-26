0aa76999a6a1168c39230afac4d8ac34a1df0a85
vgwidt <vgwidt@live.com>
2025-06-26 13:26:41 +0900

    show absolute time everywhere

--- frontend/src/components/time_format.rs
+++ frontend/src/components/time_format.rs
@@ -1,4 +1,4 @@
-use crate::hooks::use_language_context;
+use crate::{contexts::time::use_time, hooks::use_language_context};
 use yew::prelude::*;
 
 #[derive(Properties, Clone, PartialEq)]
@@ -8,6 +8,7 @@ pub struct Props {
 
 #[function_component(TimeFormat)]
 pub fn time_format(props: &Props) -> Html {
+    let time_ctx = use_time();
     let language = use_language_context();
 
     let now = chrono::Utc::now().naive_utc();
@@ -18,7 +19,12 @@ pub fn time_format(props: &Props) -> Html {
     let hours = diff.num_hours();
     let days = diff.num_days();
 
-    let timestring = {
+    let timestring = time_ctx
+        .convert_to_local(&props.time)
+        .format("%Y-%m-%d %H:%M")
+        .to_string();
+
+    let timestring_relative = {
         if minutes < 1 {
             language.get("Just now")
         } else if minutes == 1 {
@@ -44,7 +50,7 @@ pub fn time_format(props: &Props) -> Html {
 
     html! {
         <span class="timeformat">
-            {&timestring}
+            {format!("{timestring_relative} ({timestring})")}
         </span>
     }
 }
